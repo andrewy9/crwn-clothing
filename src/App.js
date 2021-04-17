9e24fd1e0c9a1deacc208b4455b009d28e735dcf
andrewy9 <andrew.yang993@gmail.com>
2021-04-17 19:11:20 +1200

    added sign-in

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+
 import ShopPage from './pages/shop/shop.component'
 import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.components';
 
 import './App.css';
 
@@ -14,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
