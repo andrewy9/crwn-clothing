8413ed9df96334d343ea8691616d46a50a334f75
andrewy9 <andrew.yang993@gmail.com>
2021-05-02 15:26:43 +1200

    added persitStore and Reducer

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { PersistGate } from 'redux-persist/integration/react';
+
 //Redux
 import { Provider } from 'react-redux'
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 //React Components
 import App from './App';
 
@@ -16,7 +18,9 @@ ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
